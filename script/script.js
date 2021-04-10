5df62f5cf2c811af287aafb626304b1580ecfafa
Raphael de Carvalho <raphaeldecarvalho1@hotmail.com>
2021-04-10 20:23:16 -0300

    alterado o campo de dias na semana para selecao em lista no script

--- script/script.js
+++ script/script.js
@@ -4,11 +4,12 @@ function calcular(){
     //tempo de consumo da energia em minutos
     tmpm = tempom.value
     //dias de consumo da energia por semana
-    tmpd = tempod.value
+    var e = document.getElementById("diassem");
+    var result = e.options[e.selectedIndex].value
     //valor cobrado na conta de energia em Kw/h
     kWth = valorKwh.value
     //tempo total de dias por semana no mês
-    tmpmes = tmpd*4.5
+    tmpmes = result*4.5
     //tempo de consumo da energia em minutos por mês
     tmpmtot = tmpm*tmpmes
     //tempo de consumo de energia por mês em horas
