0d886cd6ac2326dceca8d6284ca570cdb23faa14
Raphael de Carvalho <65046498+RaphaelDeCarvalho@users.noreply.github.com>
2021-07-14 17:51:18 -0300

    Update script.js
    Adicionado alerta de erro caso não forem colocados os dados na página.

--- script/script.js
+++ script/script.js
@@ -6,21 +6,24 @@ function calcular(){
     //dias de consumo da energia por semana
     var e = document.getElementById("diassem");
     var result = e.options[e.selectedIndex].value
-    //valor cobrado na conta de energia em Kw/h
-    kWth = valorKwh.value
-    //tempo total de dias por semana no mês
-    tmpmes = result*4.5
-    //tempo de consumo da energia em minutos por mês
-    tmpmtot = tmpm*tmpmes
-    //tempo de consumo de energia por mês em horas
-    tmph = tmpmtot/60
-    //calculo de Watt em Kilowatt
-    kWt = pot/1000
-    //calcular consumo por Kilowatts por hora
-    consum = kWt*tmph
-    //calcular valor total de consumo por mês
-    consumtot = consum*kWth
-    //Resultado convertido em Reais
-    titulo.innerHTML="R$"+consumtot.toFixed(2).replace(".",",")+" por mês."
+    if(potencia.value == 0 || tempom.value == 0 || e.options[e.selectedIndex].value == 0) {
+        alert(`[ERRO]\nDigite todos os dados!`)
+    } else {
+        //valor cobrado na conta de energia em Kw/h
+        kWth = valorKwh.value
+        //tempo total de dias por semana no mês
+        tmpmes = result*4.5
+        //tempo de consumo da energia em minutos por mês
+        tmpmtot = tmpm*tmpmes
+        //tempo de consumo de energia por mês em horas
+        tmph = tmpmtot/60
+        //calculo de Watt em Kilowatt
+        kWt = pot/1000
+        //calcular consumo por Kilowatts por hora
+        consum = kWt*tmph
+        //calcular valor total de consumo por mês
+        consumtot = consum*kWth
+        //Resultado convertido em Reais
+        titulo.innerHTML="R$"+consumtot.toFixed(2).replace(".",",")+" por mês."
+        }
     }
-    
